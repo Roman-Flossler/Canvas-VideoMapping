2674bfd1494b04638bba0d89596eded6f00a2b7b
roman.flossler <roman.flossler@eman.cz>
2022-10-30 12:18:24 +0100

    videoplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Canvas from "./Canvas";
 import "./App.css";
 
@@ -9,8 +9,12 @@ const light = new Image();
 function App() {
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const [pos, setPos] = useState<[number, number]>([500, 400]);
+  const [dragPos, setDragPos] = useState<[number, number]>([500, 400]);
   const [loads, setLoads] = useState(0);
   const [isMouseDown, setIsMouseDown] = useState(false);
+  const video = useRef<HTMLVideoElement>(null);
+  const videoHandle = useRef(0);
+  const posi = useRef([500, 400]);
 
   const someImg = "https://www.flor.cz/blog/wp-content/uploads/industrialni-portret.jpg";
 
@@ -35,13 +39,41 @@ function App() {
     light.src = "https://thumbs.gfycat.com/AchingGargantuanBass.webp";
   }, []);
 
+  function video_load_callback() {
+    if (video.current === null) {
+      return;
+    }
+    //video.current.cancelVideoFrameCallback(videoHandle.current);
+    video.current.play();
+    step();
+  }
+  function step() {
+    if (video.current === null) {
+      return;
+    }
+    console.log(posi);
+    ctx?.save();
+    ctx?.drawImage(video.current, posi.current[0] - dragPos[0], posi.current[1] - dragPos[1], 1000, 800);
+    if (ctx) {
+      ctx.globalCompositeOperation = "soft-light";
+    }
+    ctx?.drawImage(light, 0, 0, 1000, 800);
+    if (ctx) {
+      ctx.globalCompositeOperation = "destination-atop";
+    }
+    ctx?.drawImage(mask, 0, 0, 1000, 800);
+
+    videoHandle.current = video.current.requestVideoFrameCallback(step);
+    ctx?.restore();
+  }
+
   useEffect(() => {
-    render();
+    step();
   }, [loads]);
 
   const render = () => {
     ctx?.save();
-    ctx?.drawImage(initialImg, pos[0] - 500, pos[1] - 400, 1000, 800);
+    ctx?.drawImage(initialImg, pos[0] - dragPos[0], pos[1] - dragPos[1], 1000, 800);
     if (ctx) {
       ctx.globalCompositeOperation = "soft-light";
     }
@@ -57,18 +89,19 @@ function App() {
   const mouseDown = (e: React.MouseEvent) => {
     setIsMouseDown(true);
     setPos([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
+    setDragPos([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
   };
 
   const mouseUp = (e: React.MouseEvent) => {
     setIsMouseDown(false);
-    setPos([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
   };
 
   const mouseMove = (e: React.MouseEvent) => {
     if (isMouseDown) {
       setPos([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
-      console.log(pos);
-      render();
+      posi.current = [e.nativeEvent.offsetX, e.nativeEvent.offsetY];
+
+      //render();
     }
   };
 
@@ -89,6 +122,9 @@ function App() {
       >
         rotate
       </button>
+      <video onLoadedData={video_load_callback} ref={video} width="320" height="240" controls>
+        <source src="./assets/clip.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
