209f9abda98a1938a947af5aac83c58485b5dbcd
roman.flossler <roman.flossler@eman.cz>
2022-10-30 20:06:28 +0100

    konev videomapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import Canvas from "./Canvas";
 import "./App.css";
-import uluwatu from "./imgs/uluwatu.jpg";
-import mask from "./imgs/mask.png";
+import uluwatu from "./imgs/baseImg.jpg";
+import mask from "./imgs/mask2.png";
 
-const uluwatuImg = new Image();
+const baseImg = new Image();
 const maskImg = new Image();
-const screenWidth = window.screen.width;
-const screenHeight = window.screen.height - 300;
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight - 200;
 
 function App() {
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
@@ -27,10 +27,10 @@ function App() {
   };
 
   useEffect(() => {
-    uluwatuImg.onload = () => {
+    baseImg.onload = () => {
       setLoads(loads + 1);
     };
-    uluwatuImg.src = uluwatu;
+    baseImg.src = uluwatu;
 
     maskImg.onload = () => {
       setLoads(loads + 1);
@@ -41,43 +41,58 @@ function App() {
   function video_load_callback() {
     setLoads(loads + 1);
   }
+
   function play() {
     if (video.current === null) {
       return;
     }
     video.current.cancelVideoFrameCallback(videoHandle.current);
     video.current.play();
-    step();
+    renderStep();
   }
-  function step() {
+
+  function renderStep() {
     if (video.current === null) {
       return;
     }
-    ctx?.drawImage(uluwatuImg, 0, 0, ctx.canvas.offsetWidth, ctx.canvas.offsetHeight);
+    const width = ctx?.canvas.offsetWidth;
+    const height = ctx?.canvas.offsetWidth;
     ctx?.save();
+    ctx?.drawImage(baseImg, 0, 0, ctx.canvas.offsetWidth, ctx.canvas.offsetHeight);
+
+    if (ctx) {
+      ctx.globalCompositeOperation = "hard-light";
+    }
     ctx?.drawImage(
       video.current,
-      currentPos.current[0] - dragPos.current[0],
-      currentPos.current[1] - dragPos.current[1],
-      ctx.canvas.offsetWidth,
-      ctx.canvas.offsetHeight
+      currentPos.current[0] - dragPos.current[0] + 300,
+      currentPos.current[1] - dragPos.current[1] + 160,
+      ctx.canvas.offsetWidth * 0.6,
+      ctx.canvas.offsetHeight * 0.8
+    );
+    if (ctx) {
+      ctx.globalCompositeOperation = "overlay";
+      ctx.globalAlpha = 0.9;
+    }
+    ctx?.drawImage(
+      video.current,
+      currentPos.current[0] - dragPos.current[0] + 300,
+      currentPos.current[1] - dragPos.current[1] + 160,
+      ctx.canvas.offsetWidth * 0.6,
+      ctx.canvas.offsetHeight * 0.8
     );
     if (ctx) {
       ctx.globalCompositeOperation = "destination-atop";
     }
     ctx?.drawImage(maskImg, 0, 0, ctx.canvas.offsetWidth, ctx.canvas.offsetHeight);
     try {
-      videoHandle.current = video.current.requestVideoFrameCallback(step);
+      videoHandle.current = video.current.requestVideoFrameCallback(renderStep);
     } catch {
       setError("Error: your browser doesn't support requestVideoFrameCallback");
       return;
     }
     ctx?.restore();
   }
-
-  useEffect(() => {
-    //    step();
-  }, [loads]);
 
   const mouseDown = (e: React.MouseEvent) => {
     isMouseDown.current = true;
@@ -99,8 +114,10 @@ function App() {
     <div className="App">
       <div id="root">
         <div id="mouseCatcher" onMouseMove={mouseMove} onMouseDown={mouseDown} onMouseUp={mouseUp}></div>
-        <Canvas loadedImg={uluwatuImg} sizeX={screenWidth} sizeY={screenHeight} roundness={6} getCtx={getCtx}></Canvas>
+        <img src={uluwatu} alt="" />
+        <Canvas loadedImg={baseImg} sizeX={screenWidth} sizeY={screenHeight} roundness={6} getCtx={getCtx}></Canvas>
       </div>
+      <br />
       <video onLoadedData={video_load_callback} ref={video} width="60" height="40" onClick={play}>
         <source src="./assets/clip.mp4" type="video/mp4" />
       </video>
